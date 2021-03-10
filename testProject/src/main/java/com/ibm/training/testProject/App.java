4b1e327f5e62451ed143b00dc35d4f5365abc354
SakshiSuman <SakshiSuman@DESKTOP-88RHO7B>
2021-03-10 21:30:47 +0530

    First Commit

---
+++ testProject/src/main/java/com/ibm/training/testProject/App.java
@@ -0,0 +1,13 @@
+package com.ibm.training.testProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
